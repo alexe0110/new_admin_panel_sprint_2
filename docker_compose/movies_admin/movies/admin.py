41a9b69732f92539e36d209146b272c55fc87edd
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-04-13 11:19:47 +0500

    move movies_admin

---
+++ docker_compose/movies_admin/movies/admin.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from movies.models import FilmWork, Genre, GenreFilmWork, Person, PersonFilmWork
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ("full_name",)
+
+
+class GenreFilmWorkInline(admin.TabularInline):
+    model = GenreFilmWork
+    autocomplete_fields = ("genre",)
+
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = PersonFilmWork
+    autocomplete_fields = ("person",)
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmWorkInline, PersonFilmWorkInline)
+    list_prefetch_related = ("genres",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request).prefetch_related(*self.list_prefetch_related)
+        return queryset
+
+    def get_genres(self, obj):
+        return ",".join([genre.name for genre in obj.genres.all()])
+
+    get_genres.short_description = _("Жанры фильма")
+
+    # Отображение полей в списке
+    list_display = ("title", "type", "creation_date", "rating", "get_genres")
+
+    # Фильтрация в списке
+    list_filter = ("type",)
+
+    # Поиск по полям
+    search_fields = ("title", "description", "id")
