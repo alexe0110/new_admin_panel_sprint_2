bad06bae81805449053f619ba92819dae48d92f4
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-04-20 14:58:41 +0500

    cors MoviesListApi   swagger

--- config/settings.py
+++ config/settings.py
@@ -74,3 +74,4 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8080",]
--- movies/api/v1/views.py
+++ movies/api/v1/views.py
@@ -1,7 +1,37 @@
 from django.views.generic.list import BaseListView
 from django.views.generic.detail import BaseDetailView
 from django.views.generic import TemplateView
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Q
+from django.http import JsonResponse
+from django.views.generic.list import BaseListView
+
+from movies.models import FilmWork
 
 
-class MoviesListApi(TemplateView):
-    pass
+class MoviesListApi(BaseListView):
+    model = FilmWork
+    http_method_names = ['get']  # Список методов, которые реализует обработчик
+
+    def get_queryset(self):
+        """
+        Должен возвращать подготовленный QuerySet
+        """
+        return ['das','dascxxx','djgysys']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """
+        Возвращает словарь с данными для формирования страницы;
+        """
+        context = {
+            'results': list(self.get_queryset()),
+        }
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        Форматирование данных, которые вернутся при GET-запросе.
+        """
+
+        return JsonResponse(context)
+
