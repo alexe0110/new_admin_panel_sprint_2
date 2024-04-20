9ecfe9913b19a3b23b7f3a108b40c0f2b5ac95f0
a.v.cherkashin <a.v.cherkashin@tinkoff.ru>
2024-04-20 17:00:39 +0500

    mypy

--- movies/api/v1/views.py
+++ movies/api/v1/views.py
@@ -1,31 +1,39 @@
 from django.http import JsonResponse
+from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
 
 from movies.models import FilmWork
 
 
-class MoviesListApi(BaseListView):
+class MoviesApiMixin:
     model = FilmWork
-    http_method_names = ["get"]  # Список методов, которые реализует обработчик
+    http_method_names = ["get"]
 
     def get_queryset(self):
-        """
-        Должен возвращать подготовленный QuerySet
-        """
         return ["das", "dascxxx", "djgysys"]
+
+    def render_to_response(self, context, **response_kwargs):
+        return JsonResponse(context)
+
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
+    paginate_by = 50
 
     def get_context_data(self, *, object_list=None, **kwargs):
         """
         Возвращает словарь с данными для формирования страницы;
         """
-        context = {
-            "results": list(self.get_queryset()),
+        queryset = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(queryset, self.paginate_by)
+        return {
+            "count": paginator.count,
+            "total_pages": paginator.num_pages,
+            "prev": page.previous_page_number() if page.has_previous() else None,
+            "next": page.next_page_number() if page.has_next() else None,
+            "results": list(queryset),
         }
-        return context
 
-    def render_to_response(self, context, **response_kwargs):
-        """
-        Форматирование данных, которые вернутся при GET-запросе.
-        """
 
-        return JsonResponse(context)
+class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
+    def get_context_data(self, **kwargs):
+        return  # Словарь с данными объекта
